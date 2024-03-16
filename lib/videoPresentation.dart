070955f26e32b03caca9dcbc02b1eca9bdc7bbdd
OFF <peerapat13th@gmail.com>
2024-03-16 18:46:16 +0700

    week8_final without bug

--- lib/videoPresentation.dart
+++ lib/videoPresentation.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:video_url_validator/video_url_validator.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoPresentation extends StatefulWidget {
   const VideoPresentation({super.key});
@@ -8,12 +10,62 @@ class VideoPresentation extends StatefulWidget {
 }
 
 class _VideoPresentationState extends State<VideoPresentation> {
+  late YoutubePlayerController controller;
+  late String youtubePath =
+      'https://www.youtube.com/watch?v=1ukSR1GRtMU&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ';
+
+  var validator = VideoURLValidator();
+  late bool validatorStatus =
+      validator.validateYouTubeVideoURL(url: youtubePath);
+
+  @override
+  void initState() {
+    if (validatorStatus == true) {
+      controller = YoutubePlayerController(
+          initialVideoId: YoutubePlayer.convertUrlToId(youtubePath)!);
+    }
+    ;
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Video Presentation'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Card(
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      if (validatorStatus == true)
+                        YoutubePlayer(
+                            controller: controller,
+                            showVideoProgressIndicator: true,
+                            progressIndicatorColor: Colors.amber,
+                            progressColors: ProgressBarColors(
+                                playedColor: Colors.amber,
+                                handleColor: Colors.amberAccent)),
+                      if (validatorStatus == false)
+                        Center(
+                          child: Text('No Video'),
+                        )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
