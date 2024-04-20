a5dae732aadda5ad1c07d7042056abd72bf00f1f
OFF <peerapat13th@gmail.com>
2024-04-20 17:48:05 +0700

    productList

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class ProductList extends StatefulWidget {
@@ -8,14 +9,87 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
+  final dio = Dio();
+  final baseApi =
+      "https://testpos.trainingzenter.com/lab_dpu/product/list/66130151?format=json";
+  late List productList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getProduct();
+  }
+
+  Future<void> getProduct() async {
+    try {
+      await dio
+          .get(baseApi,
+              options: Options(
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json',
+                },
+              ))
+          .then((response) => {
+                if (response.statusCode == 200)
+                  {
+                    setState(() {
+                      productList = response.data!;
+                    })
+                  }
+              });
+    } catch (e) {
+      if (!context.mounted) return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(productList);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('My Product'),
+        title: Text('Product'),
       ),
-      body: Column(),
+      body: ListView.builder(
+        padding: EdgeInsets.all(8.0),
+        itemCount: productList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            clipBehavior: Clip.antiAlias,
+            color: Colors.white,
+            child: Column(
+              children: [
+                Image.network(productList[index]["product_cover"]),
+                ListTile(
+                  leading: Icon(Icons.arrow_drop_down_circle),
+                  title: Text(productList[index]["product_name"]),
+                  subtitle: Text('Price: ${productList[index]["update_date"]}'),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Updated Date ")
+                  ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(25, 177, 252, 1),
+                      ),
+                      child: Text(
+                        'More Detail',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
